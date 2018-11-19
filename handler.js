14dd0effafd65a4631ed2f65ef4adfc9608ebf45
Anh Lu <anhlucci@outlook.com>
2018-11-19 12:34:49 -0500

    deserialize

--- handler.js
+++ handler.js
@@ -10,8 +10,23 @@ const {
 
 module.exports.launch = async (event, context) => {
   getFile()
-    .then(response => response.buffer())
-    .then(response => saveToS3(response))
+    .then(res => res.buffer())
+    .then(res => saveToS3(res))
+    .then(res => lamdaToSqs())
+    .then(() => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Your function executed successfully!',
+        })
+      };
+    })
+    .catch(err => new Error(`Error scraping: ${JSON.stringify(err)}`))
+};
+
+module.exports.deserialize = async (event, context) => {
+  unzipAndParse()
+    .then(res => lamdaToSqs())
     .then(() => {
       return {
         statusCode: 200,
