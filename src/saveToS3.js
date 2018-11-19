c495d0671484b1d45530b029e2c6a322e9bf8fd5
Anh Lu <anhlucci@outlook.com>
2018-11-19 17:26:39 -0500

    new branch

--- src/saveToS3.js
+++ src/saveToS3.js
@@ -1,22 +1,29 @@
 const AWS = require('aws-sdk');
+AWS.config.setPromisesDependency(Promise);
 const s3 = new AWS.S3();
 const bucket = process.env.BUCKET_NAME;
 
 module.exports = async (event) => {
     const params = {
-        Bucket: bucket, 
-        Key: 'alliances.json.gz', 
-        Body: event}
+        Bucket: bucket,
+        Key: 'alliances.json.gz',
+        Body: event
+    }
 
-    s3.putObject(params, function (err, data) {
-        if (err) {
-            return console.log("Error", err)
-        }
-        return console.log("Success", data);
-    });
+    const putObjectPromise = s3.putObject(params).promise();
+    await putObjectPromise
+        .then(function (data) {
+            return data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
-
-
-    
-    
-
+// Simplified Callback Method
+// s3.putObject(params, function (err, data) {
+//     if (err) {
+//         return console.log("Error", err)
+//     }
+//     console.log("Success", data);
+//     return resolve();
+// });
