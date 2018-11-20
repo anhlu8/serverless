b3f5426f4805f55ddc25e6a9bed6a2f97c921ab0
Anh Lu <anhlucci@outlook.com>
2018-11-20 12:48:11 -0500

    everything's working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 module.exports = {
     getFile: require('./getFile'),
-    saveToS3: require('./saveToS3'),
-    sqsToLambda: require('./sqsToLambda'),
     lamdaToSqs: require('./lamdaToSqs'),
+    saveToS3: require('./saveToS3'),
     unzipAndParse: require('./unzipAndParse'),
-    s3ToMysql: require('./s3ToMysql')
 }
