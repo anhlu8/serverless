c1ff1174f1259e9cc45aa6254d20ed52aba9139c
Anh Lu <anhlucci@outlook.com>
2018-11-27 12:56:33 -0500

    resolvers

--- handler.js
+++ handler.js
@@ -78,14 +78,14 @@ for (let i = 0; i < urls.length; i++) {
 // }
 
 const server = new GraphQLServer({
-  typeDefs: path.join(__dirname, 'src/graphql-server/generated/prisma.graphql'),
+  typeDefs: path.join(__dirname, 'src/graphql-server/schema.graphql'), //This is schema for GraphQL API
   resolvers,
   context(req) {
     return {
       ...req,
       worldGame: worldGame,
       prisma: new Prisma({
-        typeDefs: path.join(__dirname, 'src/graphql-server/generated/prisma.graphql'),
+        typeDefs: path.join(__dirname, 'src/graphql-server/generated/prisma.graphql'), //This is schema for Prisma-binding between GraphQL API & Prisma API.
         endpoint: 'https://us1.prisma.sh/public-purplecentaur-310/prisma-graphql/dev', //This is Prisma API (same endpoint in prisma.yml)
         debug: true,
       }),
@@ -94,7 +94,7 @@ const server = new GraphQLServer({
 });
 
 server.start({ port: process.env.PORT || 4000 }, () => {
-  console.log('The server is up!')
+  console.log('The server is running at port 4000!')
 })
 
 
