dad10faa4a3641fb20d8d5b8c3b337602d1e919b
Anh Lu <anhlucci@outlook.com>
2018-12-01 17:05:37 -0500

    working on resolvers

--- src/graphql-server/index.js
+++ src/graphql-server/index.js
@@ -5,14 +5,14 @@ const { resolvers, jsonArrs } = require('../../src');
 
 const startServer = async () => {
   let db = await jsonArrs();
-  console.log('db', db)
+  console.log(db);
   const server = new GraphQLServer({
     typeDefs: './src/graphql-server/schema.graphql', //This is schema for GraphQL API
     resolvers,
-    context(req) {
+    context(req) { //context is an object with a set of properties (will get passed to every single resolver, regarless of where the resolver is defined) we can set upfor our Prisma api
       return {
         ...req,
-        db,
+        db, //this db is data from json files which are stored in S3
         prisma: new Prisma({
           typeDefs: './src/graphql-server/generated/prisma.graphql', //This is schema for Prisma-binding between GraphQL API & Prisma API.
           endpoint: 'https://us1.prisma.sh/public-purplecentaur-310/prisma-graphql/dev', //This is Prisma API (same endpoint in prisma.yml)
--- src/graphql-server/resolvers.js
+++ src/graphql-server/resolvers.js
@@ -1,22 +1,21 @@
 const resolvers = {
     Query: {
         players(parent, args, { prisma }, info) {
-            return prisma.query.players(null, info);
+            return prisma.query.players({}, info); //prisma takes 2 arguments: operations arg & selection set: in this case, operations arg is an empty object, we can also pass in 'null'
         },
         player(parent, { id }, { prisma }, info) {
-            return prisma.query.player({ id }, info);
+            return prisma.query.player({ where: {id} }, info);
         }
     },
     Mutation: {
         async createPlayer(parent, { id, nickname, habitat }, { prisma, db }, info) {
-            return prisma.mutation.createPlayer(
-                {
-                    data: {
-                        id,
-                        nickname,
-                        habitat
-                    },
+            return prisma.mutation.createPlayer({
+                data: {
+                    id:
+                    nickname,
+                    habitat
                 },
+            },
                 info
             )
         },
@@ -29,12 +28,26 @@ module.exports = resolvers;
 //     createPlayer(data:{
 //       nickname: "Anh"
 //       habitatArray:{
-//         create: {
+//         create: [
+//           {
 //           mapX:100
 //             mapY:200
 //         }
-//       }
+//           {
+//           mapX:200
+//             mapY:300
+//         }
+//           {
+//           mapX:150
+//             mapY:250
+//         }
+//         ]}
 //     }){
 //       nickname
+//       habitatArray{
+//         id
+//         mapX
+//         mapY
+//       }
 //     }
 //   }
