48cec2e86e501e9f8cff9bb247aad05e15d23f32
Anh Lu <anhlucci@outlook.com>
2018-12-03 15:57:27 -0500

    resolvers

--- src/graphql-server/resolvers.js
+++ src/graphql-server/resolvers.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { jsonArrs } = require('../../src');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const { jsonArrs } = require('../../src');
 
 const resolvers = {
     Query: {
@@ -31,7 +31,7 @@ const resolvers = {
                     i.list.map(async (j) => {
                         const dataObject = await {
                             id: j.id,
-                            nickname: j.nick
+                            nick: j.nick
                         }
                         return dataObject;
                     })
@@ -39,47 +39,51 @@ const resolvers = {
             });
             const results = await Promise.all(promises);
             return prisma.mutation.createPlayer({
-                data: {
-                    results
-                },
+                data: results
             },
                 info
             )
         },
         async createAlliance(parent, args, { prisma, db }, info){
-            await db.map(i => {
+            const promises = await db.map(i => {
                 if (i.title === '209-alliances.json') {
-                    i.list.map(j => {
-                        return prisma.mutation.createAlliance({
-                            data: {
-                                id: j.id,
-                                name: j.name,
-                                points: j.points
-                            },
-                        },
-                            info
-                        )
+                    i.list.map(async (j) => {
+                        const dataObject = await {
+                            id: j.id,
+                            name: j.name,
+                            points: j.points
+                        }
+                        return dataObject;
                     })
                 }
-            })
+            });
+            const results = await Promise.all(promises);
+            return prisma.mutation.createAlliance({
+                data: results
+            },
+                info
+            )
         },
         async createHabitat(parent, args, { prisma, db }, info){
-            await db.map(i => {
+            const promises = await db.map(i => {
                 if (i.title === '209-habitats.json') {
-                    i.list.map(j => {
-                        return prisma.mutation.createHabitat({
-                            data: {
-                                id: j.id,
-                                mapX: j.mapX,
-                                mapY: j.mapY,
-                                creationDate: j.creationDate
-                            },
-                        },
-                            info
-                        )
+                    i.list.map(async (j) => {
+                        const dataObject = await {
+                            id: j.id,
+                            mapX: j.mapX,
+                            mapY: j.mapY,
+                            creationDate: j.creationDate
+                        }
+                        return dataObject;
                     })
                 }
-            })
+            });
+            const results = await Promise.all(promises);
+            return prisma.mutation.createHabitat({
+                data: results
+            },
+                info
+            )
         },
         // async signup(parent, {email, password}, { prisma, db }, info){
         //     const encryptedPassword = await bcrypt.hash(password, 10);
